03793ac9f19961aeadc27be046babf560151bb0e
Rita <rita.lapinskaya@gmail.com>
2020-01-21 23:05:54 +0300

    Dog

---
+++ Lesson8_Construktary/src/Dog.java
@@ -0,0 +1,22 @@
+public class Dog {
+    String name;
+    int height;
+    String color;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int height) {
+        this.name = name;
+        this.height = height;
+
+    }
+
+    public Dog(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+}
